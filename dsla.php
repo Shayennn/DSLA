5c6efcdf4e78a77dbd204c275163d91709d637f0
Shayennn <me@shayennn.com>
2017-08-14 00:37:34 +0700

    Update UI by using Bootstrap 3.7.7 and Font-Awesome

--- dsla.php
+++ dsla.php
@@ -1,43 +1,75 @@
 <?php
-function datetranslate($date){
-	$monthlist=[
-		'01'=>'มกราคม',
-		'02'=>'กุมภาพันธ์',
-		'03'=>'มีนาคม',
-		'04'=>'เมษายน',
-		'05'=>'พฤษภาคม',
-		'06'=>'มิถุนายน',
-		'07'=>'กรกฎาคม',
-		'08'=>'สิงหาคม',
-		'09'=>'กันยายน',
-		'10'=>'ตุลาคม',
-		'11'=>'พฤศจิกายน',
-		'12'=>'ธันวาคม',
-	];
-	$ts=strtotime($date);
-	$return['day']=date('j',$ts);
-	$return['month']=$monthlist[date('m',$ts)];
-	$return['year']=543+date('Y',$ts);
-	return $return;
+function randomphonenumber(){
+	return '0'.rand(800000000,899999999);
+}
+function datetranslate($date)
+{
+    $monthlist=[
+        '01'=>'มกราคม',
+        '02'=>'กุมภาพันธ์',
+        '03'=>'มีนาคม',
+        '04'=>'เมษายน',
+        '05'=>'พฤษภาคม',
+        '06'=>'มิถุนายน',
+        '07'=>'กรกฎาคม',
+        '08'=>'สิงหาคม',
+        '09'=>'กันยายน',
+        '10'=>'ตุลาคม',
+        '11'=>'พฤศจิกายน',
+        '12'=>'ธันวาคม',
+    ];
+    $ts=strtotime($date);
+    $return['day']=date('j', $ts);
+    $return['month']=$monthlist[date('m', $ts)];
+    $return['year']=543+date('Y', $ts);
+    return $return;
 }
 
 header('Content-type: image/png');
 date_default_timezone_set("Asia/Bangkok");
 
-if($_GET['stuid']=='')$_GET['stuid']='09999';
-if($_GET['name']=='')$_GET['name']='นาย ขยัน หมันเพียร';
-if($_GET['class_a']=='')$_GET['class_a']='1';
-if($_GET['class_b']=='')$_GET['class_b']='10';
-if($_GET['attachment']=='')$_GET['attachment']='-';
-if($_GET['tel']=='')$_GET['tel']='0812345678';
-if($_GET['writedate']=='')$_GET['writedate']=date('Y-m-d');
-if($_GET['latype']=='')$_GET['latype']='ป่วย';
-if($_GET['reason']=='')$_GET['reason']='ตัวร้อน ไข้ขึ้น';
-if($_GET['lastart']=='')$_GET['lastart']=date('Y-m-d',strtotime("-2 days"));
-if($_GET['laend']=='')$_GET['laend']=date('Y-m-d',strtotime("-1 days"));
-if($_GET['parent']=='')$_GET['parent']='นาย ประหยัด หมั่นเพียร';
-if($_GET['parent_comment']=='')$_GET['parent_comment']='เห็นสมควรให้ลา';
-if($_GET['adv']=='')$_GET['adv']='อาจารย์ อัมรินทร์ อินทร์อยู่';
+if (!isset($_GET['stuid'])||$_GET['stuid']=='') {
+    $_GET['stuid']='09999';
+}
+if (!isset($_GET['name'])||$_GET['name']=='') {
+    $_GET['name']='นาย ขยัน หมันเพียร';
+}
+if (!isset($_GET['class_a'])||$_GET['class_a']=='') {
+    $_GET['class_a']='1';
+}
+if (!isset($_GET['class_b'])||$_GET['class_b']=='') {
+    $_GET['class_b']='10';
+}
+if (!isset($_GET['attachment'])||$_GET['attachment']=='') {
+    $_GET['attachment']='-';
+}
+if (!isset($_GET['tel'])||$_GET['tel']=='') {
+    $_GET['tel']=randomphonenumber();
+}
+if (!isset($_GET['writedate'])||$_GET['writedate']=='') {
+    $_GET['writedate']=date('Y-m-d');
+}
+if (!isset($_GET['latype'])||$_GET['latype']=='') {
+    $_GET['latype']='ป่วย';
+}
+if (!isset($_GET['reason'])||$_GET['reason']=='') {
+    $_GET['reason']='ตัวร้อน ไข้ขึ้น';
+}
+if (!isset($_GET['lastart'])||$_GET['lastart']=='') {
+    $_GET['lastart']=date('Y-m-d', strtotime("-2 days"));
+}
+if (!isset($_GET['laend'])||$_GET['laend']=='') {
+    $_GET['laend']=date('Y-m-d', strtotime("-1 days"));
+}
+if (!isset($_GET['parent'])||$_GET['parent']=='') {
+    $_GET['parent']='นาย ประหยัด หมั่นเพียร';
+}
+if (!isset($_GET['parent_comment'])||$_GET['parent_comment']=='') {
+    $_GET['parent_comment']='เห็นสมควรให้ลา';
+}
+if (!isset($_GET['adv'])||$_GET['adv']=='') {
+    $_GET['adv']='อาจารย์ อัมรินทร์ อินทร์อยู่';
+}
 
 $lacount=(strtotime($_GET['laend'])-strtotime($_GET['lastart']))/(3600*24)+1;
 
@@ -45,38 +77,37 @@ $_GET['writedate']=datetranslate($_GET['writedate']);
 $_GET['lastart']=datetranslate($_GET['lastart']);
 $_GET['laend']=datetranslate($_GET['laend']);
 
-$font_path = '/var/www/vhosts/shayennn.com/httpdocs/dsla/THSarabunNew.ttf';
+$font_path = 'THSarabunNew.ttf';
 $jpg_image = imagecreatefromjpeg('dsla.jpg');
 $black = imagecolorallocate($jpg_image, 0, 0, 0);
 
-imagettftext($jpg_image,39, 0, 560, 967, $black, $font_path, $_GET['attachment']);
-imagettftext($jpg_image,39, 0, 541, 1137, $black, $font_path, $_GET['stuid']);
-imagettftext($jpg_image,39, 0, 1828, 1052, $black, $font_path, $_GET['class_a']);
-imagettftext($jpg_image,39, 0, 1953, 1052, $black, $font_path, $_GET['class_b']);
-imagettftext($jpg_image,39, 0, 683, 1052, $black, $font_path, $_GET['name']);
-imagettftext($jpg_image,39, 0, 1041, 1801, $black, $font_path, $_GET['name']);
-imagettftext($jpg_image,39, 0, 1234, 1384, $black, $font_path, $_GET['tel']);
-imagettftext($jpg_image,39, 0, 1448, 717, $black, $font_path, $_GET['writedate']['day']);
-imagettftext($jpg_image,39, 0, 1691, 717, $black, $font_path, $_GET['writedate']['month']);
-imagettftext($jpg_image,39, 0, 2094, 717, $black, $font_path, $_GET['writedate']['year']);
-imagettftext($jpg_image,39, 0, 497, 801, $black, $font_path, $_GET['latype']);
-if($_GET['latype']=='กิจ'){
-	imagettftext($jpg_image,60, 0, 1772, 1137, $black, $font_path, '/');
-}else{
-	imagettftext($jpg_image,60, 0, 1197, 1137, $black, $font_path, '/');
+imagettftext($jpg_image, 44, 0, 560, 967, $black, $font_path, $_GET['attachment']);
+imagettftext($jpg_image, 44, 0, 541, 1137, $black, $font_path, $_GET['stuid']);
+imagettftext($jpg_image, 44, 0, 1828, 1052, $black, $font_path, $_GET['class_a']);
+imagettftext($jpg_image, 44, 0, 1953, 1052, $black, $font_path, $_GET['class_b']);
+imagettftext($jpg_image, 44, 0, 683, 1052, $black, $font_path, $_GET['name']);
+imagettftext($jpg_image, 44, 0, 1041, 1801, $black, $font_path, $_GET['name']);
+imagettftext($jpg_image, 44, 0, 1234, 1384, $black, $font_path, $_GET['tel']);
+imagettftext($jpg_image, 44, 0, 1448, 717, $black, $font_path, $_GET['writedate']['day']);
+imagettftext($jpg_image, 44, 0, 1691, 717, $black, $font_path, $_GET['writedate']['month']);
+imagettftext($jpg_image, 44, 0, 2094, 717, $black, $font_path, $_GET['writedate']['year']);
+imagettftext($jpg_image, 44, 0, 497, 801, $black, $font_path, $_GET['latype']);
+if ($_GET['latype']=='กิจ') {
+    imagettftext($jpg_image, 70, 0, 1772, 1137, $black, $font_path, '/');
+} else {
+    imagettftext($jpg_image, 70, 0, 1197, 1137, $black, $font_path, '/');
 }
-imagettftext($jpg_image,39, 0, 474, 1220, $black, $font_path, $_GET['reason']);
-imagettftext($jpg_image,39, 0, 1762, 1220, $black, $font_path, $_GET['lastart']['day']);
-imagettftext($jpg_image,39, 0, 325, 1303, $black, $font_path, $_GET['lastart']['month']);
-imagettftext($jpg_image,39, 0, 713, 1303, $black, $font_path, $_GET['lastart']['year']);
-imagettftext($jpg_image,39, 0, 985, 1303, $black, $font_path, $_GET['laend']['day']);
-imagettftext($jpg_image,39, 0, 1234, 1303, $black, $font_path, $_GET['laend']['month']);
-imagettftext($jpg_image,39, 0, 1568, 1303, $black, $font_path, $_GET['laend']['year']);
-imagettftext($jpg_image,39, 0, 1954, 1303, $black, $font_path, $lacount);
-imagettftext($jpg_image,39, 0, 986, 1885, $black, $font_path, $_GET['parent_comment']);
-imagettftext($jpg_image,39, 0, 1041, 2053, $black, $font_path, $_GET['parent']);
-imagettftext($jpg_image,39, 0, 1041, 2302, $black, $font_path, $_GET['adv']);
+imagettftext($jpg_image, 44, 0, 474, 1220, $black, $font_path, $_GET['reason']);
+imagettftext($jpg_image, 44, 0, 1762, 1220, $black, $font_path, $_GET['lastart']['day']);
+imagettftext($jpg_image, 44, 0, 325, 1303, $black, $font_path, $_GET['lastart']['month']);
+imagettftext($jpg_image, 44, 0, 713, 1303, $black, $font_path, $_GET['lastart']['year']);
+imagettftext($jpg_image, 44, 0, 985, 1303, $black, $font_path, $_GET['laend']['day']);
+imagettftext($jpg_image, 44, 0, 1234, 1303, $black, $font_path, $_GET['laend']['month']);
+imagettftext($jpg_image, 44, 0, 1568, 1303, $black, $font_path, $_GET['laend']['year']);
+imagettftext($jpg_image, 44, 0, 1954, 1303, $black, $font_path, $lacount);
+imagettftext($jpg_image, 44, 0, 986, 1885, $black, $font_path, $_GET['parent_comment']);
+imagettftext($jpg_image, 44, 0, 1041, 2053, $black, $font_path, $_GET['parent']);
+imagettftext($jpg_image, 44, 0, 1041, 2302, $black, $font_path, $_GET['adv']);
 
 imagepng($jpg_image);
 imagedestroy($jpg_image);
-?>
--- index.php
+++ index.php
@@ -1,74 +1,128 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8">
-    <title>DSLA Image</title>
-  </head>
-  <body>
-	<h1>ระบบพิมพ์ใบลา</h1>
-	<form action="dsla.php">
-		<p>
-			<label for="stuid">รหัสนักเรียน</label>
-			<input type="text" id="stuid" name="stuid" value="">
-		</p>
-		<p>
-			<label for="name">ชื่อ</label>
-			<input type="text" id="name" name="name" value="">
-		</p>
-		<p>
-			<label for="class_a">ระดับชั้น</label>
-			<input type="text" id="class_a" name="class_a" value="6">
-		</p>
-		<p>
-			<label for="class_b">ห้อง</label>
-			<input type="text" id="class_b" name="class_b" value="2">
-		</p>
-		<p>
-			<label for="tel">เบอร์โทร</label>
-			<input type="text" id="tel" name="tel" value="">
-		</p>
-		<p>
-			<label for="writedate">วันที่เขียน</label>
-			<input type="date" id="writedate" name="writedate" value="<?php echo date("Y-m-d");?>">
-		</p>
-		<p>
-			<label for="">ประเภทลา</label>
-			<input type="radio" id="latype_pouy" name="latype" value="ป่วย" checked><label>ป่วย</label>
-			<input type="radio" id="latype_kij" name="latype" value="กิจ"><label>กิจ</label>
-		</p>
-		<p>
-			<label for="">สิ่งที่แนบมาด้วย</label>
-			<input type="radio" id="attachment_none" name="attachment" value="-" checked><label>ไม่มี</label>
-			<input type="radio" id="attachment_doctor" name="attachment" value="ใบรับรองแพทย์"><label>ใบรับรองแพทย์</label>
-			<input type="radio" id="attachment_document" name="attachment" value="เอกสารประกอบ"><label>เอกสารประกอบ</label>
-		</p>
-		<p>
-			<label for="reason">สาเหตุของการลา</label>
-			<input type="text" id="reason" name="reason" value="ตัวร้อน ไข้ขึ้น">
-		</p>
-		<p>
-			<label for="lastart">ลาตั้งแต่วันที่</label>
-			<input type="date" id="lastart" name="lastart" value="<?php echo date("Y-m-d",strtotime("-2 days"));?>">
-		</p>
-		<p>
-			<label for="laend">ถึงวันที่</label>
-			<input type="date" id="laend" name="laend" value="<?php echo date("Y-m-d",strtotime("-1 days"));?>">
-		</p>
-		<p>
-			<label for="parent_comment">ความเห็นผู้ปกครอง</label>
-			<input type="text" id="parent_comment" name="parent_comment" value="เห็นสมควรให้ลา">
-		</p>
-		<p>
-			<label for="parent">ผู้ปกครองชื่อ</label>
-			<input type="text" id="parent" name="parent" value="">
-		</p>
-		<p>
-			<label for="adv">ที่ปรึกษาชื่อ</label>
-			<input type="text" id="adv" name="adv" value="">
-		</p>
-		<button type="submit">สร้างภาพ</button>
-		<br><br>
-		<img id="img" src="dsla.php" style="width:40%" alt="" />
-	</form>
-  </body>
+
+<head>
+	<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+	<script src="https://use.fontawesome.com/c259a54250.js"></script>
+	<meta charset="utf-8">
+	<title>DSLA Image</title>
+</head>
+
+<body>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="page-header">
+					<h1>ระบบพิมพ์ใบลาโรงเรียนสาธิตฯ</h1>
+				</div>
+
+				<form action="dsla.php" class="form-horizontal">
+					<div class="form-group">
+						<label class="col-sm-2 control-label" for="stuid">รหัสนักเรียน</label>
+						<div class="col-sm-10"><input class="form-control" type="text" id="stuid" name="stuid" value=""></div>
+					</div>
+					<div class="form-group">
+						<label class="col-sm-2 control-label" for="name">ชื่อ</label>
+						<div class="col-sm-10"><input class="form-control" type="text" id="name" name="name" value=""></div>
+					</div>
+					<div class="form-group">
+						<label class="col-sm-2 control-label" for="class_a">ระดับชั้น</label>
+						<div class="col-sm-10"><input class="form-control" type="text" id="class_a" name="class_a" value="6"></div>
+					</div>
+					<div class="form-group">
+						<label class="col-sm-2 control-label" for="class_b">ห้อง</label>
+						<div class="col-sm-10"><input class="form-control" type="text" id="class_b" name="class_b" value="2"></div>
+					</div>
+					<div class="form-group">
+						<label class="col-sm-2 control-label" for="tel">เบอร์โทร</label>
+						<div class="col-sm-10"><input class="form-control" type="text" id="tel" name="tel" value=""></div>
+					</div>
+					<div class="form-group">
+						<label class="col-sm-2 control-label" for="writedate">วันที่เขียน</label>
+						<div class="col-sm-10"><input class="form-control" type="date" id="writedate" name="writedate" value="<?php echo date(" Y-m-d ");?>"></div>
+					</div>
+					<div class="form-group">
+						<label class="col-sm-2 control-label" for="">ประเภทลา</label>
+						<div class="col-sm-10">
+							<div class="radio">
+								<label><input type="radio" id="latype_pouy" name="latype" value="ป่วย" checked> ป่วย</label>
+							</div>
+							<div class="radio">
+								<label><input type="radio" id="latype_kij" name="latype" value="กิจ"> กิจ</label>
+							</div>
+						</div>
+					</div>
+					<div class="form-group">
+						<label class="col-sm-2 control-label" for="">สิ่งที่แนบมาด้วย</label>
+						<div class="col-sm-10">
+							<div class="radio">
+								<label><input type="radio" id="attachment_none" name="attachment" value="-" checked> ไม่มี</label>
+							</div>
+							<div class="radio">
+								<label><input type="radio" id="attachment_doctor" name="attachment" value="ใบรับรองแพทย์"> ใบรับรองแพทย์</label>
+							</div>
+							<div class="radio">
+								<label><input type="radio" id="attachment_document" name="attachment" value="เอกสารประกอบ"> เอกสารประกอบ</label>
+							</div>
+						</div>
+					</div>
+					<div class="form-group">
+						<label class="col-sm-2 control-label" for="reason">สาเหตุของการลา</label>
+						<div class="col-sm-10"><input class="form-control" type="text" id="reason" name="reason" value="ตัวร้อน ไข้ขึ้น"></div>
+					</div>
+					<div class="form-group">
+						<label class="col-sm-2 control-label" for="lastart">ลาตั้งแต่วันที่</label>
+						<div class="col-sm-10"><input class="form-control" type="date" id="lastart" name="lastart" value="<?php echo date(" Y-m-d ", strtotime("-2 days "));?>"></div>
+					</div>
+					<div class="form-group">
+						<label class="col-sm-2 control-label" for="laend">ถึงวันที่</label>
+						<div class="col-sm-10"><input class="form-control" type="date" id="laend" name="laend" value="<?php echo date(" Y-m-d ", strtotime("-1 days "));?>"></div>
+					</div>
+					<div class="form-group">
+						<label class="col-sm-2 control-label" for="parent_comment">ความเห็นผู้ปกครอง</label>
+						<div class="col-sm-10"><input class="form-control" type="text" id="parent_comment" name="parent_comment" value="เห็นสมควรให้ลา"></div>
+					</div>
+					<div class="form-group">
+						<label class="col-sm-2 control-label" for="parent">ผู้ปกครองชื่อ</label>
+						<div class="col-sm-10"><input class="form-control" type="text" id="parent" name="parent" value=""></div>
+					</div>
+					<div class="form-group">
+						<label class="col-sm-2 control-label" for="adv">ที่ปรึกษาชื่อ</label>
+						<div class="col-sm-10"><input class="form-control" type="text" id="adv" name="adv" value=""></div>
+					</div>
+					<div class="form-group">
+						<div class="col-sm-10 col-sm-offset-2">
+							<div class="col-sm-10"><button type="submit" class="btn btn-success">Generate ใบลา</button></div>
+						</div>
+					</div>
+				</form>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12">
+				<div class="page-header">
+					<h1>ตัวอย่างใบลาจากระบบ</h1>
+				</div>
+				<img id="img" src="dsla.php" class="img-responsive" />
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12">
+				<p class="text-center">Just a easy system. <a href="https://fb.me/Phitchawat.L">Shayennn</a></p>
+				<p class="text-center"><i class="fa fa-github" aria-hidden="true"></i> <a href="https://github.com/Shayennn/DSLA">Shayennn/DSLA</a></p>
+			</div>
+		</div>
+	</div>
+</body>
+
 </html>
